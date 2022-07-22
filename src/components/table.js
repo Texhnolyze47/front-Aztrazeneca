0f86457c300c10b586d974f0a387a87a6e7ae954
Texhnolyze <ichigobankai136@gmail.com>
2022-07-22 17:20:31 -0500

    add: se termino la tabla

--- src/components/table.js
+++ src/components/table.js
@@ -17,7 +17,35 @@ function Table (){
         <div className="container">
             <h2> Tabla de datos</h2>
 
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title Name</th>
+                        <th>Last Refresh</th>
+                        <th>Recruiting</th>
+                        <th>Url Site</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {Estudios.map((item,i) => {
+                    return <tr>
+                        <td>{item.id}</td>
+                        <td>{item.titleName}</td>
+                        <td>{item.lastRefresh}</td>
+                        <td>{item.urlSite}</td>
+                        <td>{item.recruiting}</td>
+                    </tr>
+                })}
+                <tr>
+
+                </tr>
+                </tbody>
+    
+
+            </table>
         </div>
+
     )
 }
 
