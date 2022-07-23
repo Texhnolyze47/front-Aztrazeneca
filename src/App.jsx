943b058dae96ee37964ac4117bd78115368e193b
Texhnolyze <ichigobankai136@gmail.com>
2022-07-23 00:50:20 -0500

    add: tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from './routes/Home'
 import Navigation from './components/Navigation'
 import Cabecero from "./components/cabecero";
+import Table from "./components/table";
 
 import { Link } from "react-router-dom";
 
@@ -11,7 +12,8 @@ export default function App(){
         <div>
              <Cabecero />
             <Navigation />
-           
+            <Table />
+                       
         </div>
 
        
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,28 @@
 import { Link } from "react-router-dom";
+import './Navigation.css';
 
 export default function Navigation() {
     return (
            <div>
              <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>
+                <ul className="listas">
+                    <li >
+                        <Link className="listas-links" to='/'>
                             Home
                         </Link>
                     </li>
-                    <li>
-                        <Link to='/Date'>
+                    <li >
+                        <Link className="listas-links" to='/Date'>
                             Data
                         </Link>
                     </li>
-                    <li>
-                        <Link to='/Maps'>
+                    <li >
+                        <Link className="listas-links" to='/Maps'>
                             Maps
                         </Link>
                     </li>
-                    <li>
-                        <Link to='/Contact'>
+                    <li >
+                        <Link className="listas-links"  to='/Contact'>
                             Contact
                         </Link>
                     </li>
